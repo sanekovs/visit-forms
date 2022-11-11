b3205821ab58d651b9d0f33390854a652423ce7c
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-11-11 10:04:26 +0100

    chore: add distros for demo purposes

---
+++ packages/form-js/dist/index.cjs
@@ -0,0 +1,47 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var formJsViewer = require('@bpmn-io/form-js-viewer');
+var formJsEditor = require('@bpmn-io/form-js-editor');
+var formJsPlayground = require('@bpmn-io/form-js-playground');
+
+
+
+Object.defineProperty(exports, 'Form', {
+	enumerable: true,
+	get: function () {
+		return formJsViewer.Form;
+	}
+});
+Object.defineProperty(exports, 'createForm', {
+	enumerable: true,
+	get: function () {
+		return formJsViewer.createForm;
+	}
+});
+Object.defineProperty(exports, 'getSchemaVariables', {
+	enumerable: true,
+	get: function () {
+		return formJsViewer.getSchemaVariables;
+	}
+});
+Object.defineProperty(exports, 'FormEditor', {
+	enumerable: true,
+	get: function () {
+		return formJsEditor.FormEditor;
+	}
+});
+Object.defineProperty(exports, 'createFormEditor', {
+	enumerable: true,
+	get: function () {
+		return formJsEditor.createFormEditor;
+	}
+});
+Object.defineProperty(exports, 'FormPlayground', {
+	enumerable: true,
+	get: function () {
+		return formJsPlayground.Playground;
+	}
+});
+//# sourceMappingURL=index.cjs.map
